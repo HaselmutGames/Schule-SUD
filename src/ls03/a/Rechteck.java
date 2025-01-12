f0c024b8496f795b5112ea9dcaa86764901fe4f4
HaselmutGames <haselmut.games@gmail.com>
2025-01-12 14:07:53 +0100

    Update JavaDoc Rechteck.class

--- src/ls03/a/Rechteck.java
+++ src/ls03/a/Rechteck.java
@@ -1,11 +1,33 @@
 /**
  * @author Hammoud Aldalati
  * @version 03.12.2024
+ * @version 12.01.2025 
  *
- * Implementierung der Klasse Rechteck mit 2 Attributen/Eigenschaften
- * laenge und breite
- * Entsprechend sind getter und setter fuer die Attribute vorhanden
- * Zur Berechnung von Flaeche und Umfang werden die Methoden aus der abstrakten Klasse FigurZweiDimensional implementiert
+ * * Implementierung der Klasse Rechteck
+ * 
+ * * Die Klasse Rechteck erbt von der abstrakten Klasse FigurZweiDimensional
+ * * zur Berechnung von Flaeche und Umfang werden die Methoden aus der abstrakten Klasse FigurZweiDimensional implementiert
+ * 
+ * * Die Klasse Rechteck hat 2 Attribute: laenge und breite
+ * 
+ * * Die Klasse Rechteck hat 2 Konstruktoren:
+ * * 1. Konstruktor: Rechteck(double laenge, double breite)
+ * * 2. Copy-Konstruktor: Rechteck(Rechteck rechteck)
+ * 
+ * * Die Klasse Rechteck hat 2 getter und 2 setter Methoden:
+ * * 1. getter: getLaenge()
+ * * 2. getter: getBreite()
+ * * 3. setter: setLaenge(double laenge)
+ * * 4. setter: setBreite(double breite)
+ * 
+ * * Die Klasse Rechteck hat 3 Methoden:
+ * * 1. Methode: berechneFlaeche() - berechnet die Flaeche des Rechtecks
+ * * 2. Methode: berechneUmfang() - berechnet den Umfang des Rechtecks
+ * * 3. Methode: toString() - gibt die Attribute des Rechtecks als String zurueck
+ * 
+ * * Fehlerbehandlung:  Ueberprueft die Laenge und Breite direkt beim Erstellen
+ *      * Wenn die Laenge oder Breite kleiner oder gleich 0 ist, wird eine IllegalArgumentException geworfen
+ *
  * */
 package ls03.a;
 
