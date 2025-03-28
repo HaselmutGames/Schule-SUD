bf52ba908e3e773e959a8afdc7ecef6c600cba20
HaselmutGames <haselmut.games@gmail.com>
2025-03-28 22:50:17 +0100

    Implement main method in Verwaltung

--- src/ls04/Verwaltung.java
+++ src/ls04/Verwaltung.java
@@ -3,7 +3,10 @@ package ls04;
 import java.util.ArrayList;
 import java.util.List;
 
+import ls04.mitarbeiter.Bueroarbeiter;
+import ls04.mitarbeiter.Manager;
 import ls04.mitarbeiter.Mitarbeiter;
+import ls04.mitarbeiter.Schichtarbeiter;
 
 public class Verwaltung {
     private List<Mitarbeiter> mitarbeiterListe = new ArrayList<>();
@@ -27,5 +30,19 @@ public class Verwaltung {
             summe += mitarbeiter.einkommen();
         }
         System.out.println("Gesamtauszahlung: " + summe);
+    }
+
+    public static void main(String[] args) {
+        Verwaltung verwaltung = new Verwaltung();
+        
+        Schichtarbeiter schichtarbeiter1 = new Schichtarbeiter(3001, "Hola", 15.0, 160);
+        Bueroarbeiter bueroarbeiter1 = new Bueroarbeiter(5002, "Hello", 3000.0);
+        Manager manager1 = new Manager(2003, "Hallo", 5000.0, 10);
+
+        verwaltung.neuerMitarbeiter(schichtarbeiter1);
+        verwaltung.neuerMitarbeiter(bueroarbeiter1);
+        verwaltung.neuerMitarbeiter(manager1);
+
+        verwaltung.gehaltslisteErstellen();
     }
 }
