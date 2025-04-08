75d33af218568955c1b760de65ec98795b0ef93a
HaselmutGames <haselmut.games@gmail.com>
2025-04-08 16:58:20 +0200

    Update constructor for Manager class with new ID range

--- src/ls04/mitarbeiter/Manager.java
+++ src/ls04/mitarbeiter/Manager.java
@@ -6,8 +6,12 @@ public class Manager extends Mitarbeiter {
 
     public Manager(int id, String name, double festgehalt, double bonusProzent) {
         super(id, name);
-        if(id < 1000 || id > 2999){
-            throw new IllegalArgumentException("Manager-ID muss vierstellig und positiv sein.");
+        if (!((id >= 1000 && id <= 2999) || (id >= 4000 && id <= 4999) || (id >= 6000 && id <= 9999))) { // Erlaubt:
+                                                                                                         // 1000-2999,
+                                                                                                         // 4000-4999,
+                                                                                                         // 6000-9999
+            throw new IllegalArgumentException(
+                    "Manager-ID muss vierstellig und in einem erlaubten Bereich liegen (1000-2999, 4000-4999, 6000-9999).");
         }
         this.festgehalt = festgehalt;
         this.bonusProzent = bonusProzent;
